18efbcc23bec76692bba798cb5f3e91f27173d7b
ericbattiston <eric.battiston@gmail.com>
2013-11-06 19:21:31 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CoffeeForward::Application.config.secret_key_base = '1f4ed33fd06841aea4dbf28dea7151ad70f23e8c17c6360f6e3706ef08dd056ea8e1cf2856a834c0e8d22d6e094f2b2b20c1bc6e804f9e27e3f9dc7ef1474be3'
