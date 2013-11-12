98ddf0a7f462f2ca6d836ef873ceca3f699e699a
ericbattiston <eric.battiston@gmail.com>
2013-11-12 15:15:50 -0500

    Edit Description
    Changed the prewritted readme.doc description to better reflect the
    program

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 CoffeeForward::Application.routes.draw do
+  get "gifts/index"
+  get "gifts/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
